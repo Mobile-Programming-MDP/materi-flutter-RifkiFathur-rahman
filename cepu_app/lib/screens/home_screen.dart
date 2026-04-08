dda7d01f77edb90fe7db5b4df8a3be092c746940
RifkiFathurahman <rifkifathurrahman1@mhs.mdp.ac.id>
2026-04-08 12:34:56 +0700

    avatars ui

--- cepu_app/lib/screens/home_screen.dart
+++ cepu_app/lib/screens/home_screen.dart
@@ -10,32 +10,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Future<void> signOut() async{
+  Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
-    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => SignInScreen()), (route) =>  true);
-
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => SignInScreen()),
+      (route) => true,
+    );
   }
+
   String? _idToken = "";
   String? _uid = "";
   String? _email = "";
 
-  Future <void> getFirebaseAuthUser() async {
+  Future<void> getFirebaseAuthUser() async {
     User? user = FirebaseAuth.instance.currentUser;
 
-    if (user != null){
+    if (user != null) {
       _uid = user.uid;
       _email = user.email;
       await user
           .getIdToken(true)
           .then(
             (value) => {
-            setState(() {
-              _idToken = value;
-            }),
-          },
-        );
+              setState(() {
+                _idToken = value;
+              }),
+            },
+          );
     }
+  }
 
+  String generateAvatarUrl(String? fullname) {
+    final formattedName = fullname!.trim().replaceAll(' ', '+');
+    return ' https://ui-avatars.com/api/?name-$formattedName&color-7F9CF5&background-EBF4FF';
   }
 
   @override
@@ -47,10 +55,23 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home Screen"), actions: [
-        IconButton(onPressed: signOut, icon: Icon(Icons.logout))
-      ],),
-      body: const Center(child: Text("You Have Been Signed In!")),
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        actions: [IconButton(onPressed: signOut, icon: Icon(Icons.logout))],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text("Home Screen"),
+            Image.network(
+              generateAvatarUrl(
+                // FirebaseAuth.instance.currentUser?.displayName.toString(),
+                "jefli",
+              ).toString(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
